398736dedcaef2b5670820f408e6be514c7a8057
tommya98 <tommya98@naver.com>
2023-09-02 20:28:50 +0900

    FEAT: 버튼 컴포넌트 구현

---
+++ blog-practice/src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,22 @@
+import { StoryFn, Meta } from '@storybook/react';
+import { Button } from '.';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+} as Meta<typeof Button>;
+
+const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
+
+export const RedButton = Template.bind({});
+
+RedButton.args = {
+  label: '등록',
+};
+
+export const BlueButton = Template.bind({});
+
+BlueButton.args = {
+  label: '닫기',
+  color: '#304FFE',
+};
