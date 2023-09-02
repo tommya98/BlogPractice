041572f3400d2a715f5a6dddee4ccb38b8ab00da
tommya98 <tommya98@naver.com>
2023-09-02 20:33:41 +0900

    FEAT: 다이얼로그 타이틀 컴포넌트 구현

---
+++ blog-practice/src/components/atoms/DialogTitle/index.stories.tsx
@@ -0,0 +1,15 @@
+import { StoryFn, Meta } from '@storybook/react';
+import { DialogTitle } from '.';
+
+export default {
+  title: 'Atoms/DialogTitle',
+  component: DialogTitle,
+} as Meta<typeof DialogTitle>;
+
+const Template: StoryFn<typeof DialogTitle> = (args) => <DialogTitle {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: '블로그 글 등록',
+};
